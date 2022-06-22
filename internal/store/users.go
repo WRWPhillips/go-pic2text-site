229121cac729f99cc288cbb7df768a6e29c3a029
WRWPhillips <42WPhillips42@gmail.com>
2022-06-22 17:38:19 -0500

    Migrations made, users table in db updating

--- internal/store/users.go
+++ internal/store/users.go
@@ -6,6 +6,8 @@ type User struct {
   ID       int
   Username string `binding:"required,min=5,max=30"`
   Password string `binding:"required,min=7,max=32"`
+  CreatedAt  time.Time
+  ModifiedAt time.Time
 }
  
 func AddUser(user *User) error {
