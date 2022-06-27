cf14d7a252296c8ba0c70dd742419cacf016cdbd
WRWPhillips <42WPhillips42@gmail.com>
2022-06-27 12:12:17 -0500

    jwt.go created, JWT issuance next

---
+++ internal/server/jwt.go
@@ -0,0 +1,28 @@
+package server
+
+import (
+	"github.com/WRWPhillips/go-pic2text-site/internal/conf"
+
+	"github.com/cristalhq/jwt/v3"
+	"github.com/rs/zerolog/log"
+)
+
+var (
+	jwtSigner   jwt.Signer
+	jwtVerifier jwt.Verifier
+)
+
+func jwtSetup(conf conf.Config) {
+	var err error 
+	key := []byte(conf.JwtSecret)
+
+	jwtSigner, err = jwt.NewSignerHS(jwt.HS256, key)
+	if err != nil {
+		log.Panic().Err(err).Msg("Error creating JWT signer")
+	}
+
+	jwtVerifier, err = jwt.NewVerifierHS(jwt.HS256, key)
+	if err != nil {
+		log.Panis().Err(err).Msg("Error creating JWT verifier")
+	}
+}
